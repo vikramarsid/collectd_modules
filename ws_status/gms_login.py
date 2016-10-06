049296461b6ba4e38923a9ce7a2b260447842da5
santy2509 <santosh.vy1144@gmail.com>
2016-10-06 13:47:51 -0700

    Remove Associated IP's

--- ws_status/gms_login.py
+++ ws_status/gms_login.py
@@ -1,17 +1,26 @@
 import requests, json
-
+from requests import ConnectionError
 headers = {"Content-Type": "application/json"}
 payload = {"user": "admin", "password": "admin"}
 
 
 def gms_login(url):
-    p = requests.post('https://'+url+'/gms/rest/authentication/login', data=json.dumps(payload), headers=headers,
+    try:
+        p = requests.post('https://' + url + '/gms/rest/authentication/login', data=json.dumps(payload),
+                          headers=headers,
                       verify=False)
-    id = p.headers['Set-Cookie'].split(";")
-    gms_session_id = id[0]
-    #print p.status_code
-    print p.text
-    # print gmsSessionID
+        id = p.headers['Set-Cookie'].split(";")
+        gms_session_id = id[0]
+        # print p.status_code
+        print p.text
+        # print gmsSessionID
+
+        return gms_session_id
+
+    except ConnectionError:
+        print " Invalid GMS_IP"
+
+
 
     return gms_session_id
 
